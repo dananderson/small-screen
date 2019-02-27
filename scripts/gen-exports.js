aa7556494d3540c0d60ee040c4f07dffb55efdb8
Daniel Anderson <tvengr@gmail.com>
2019-02-26 23:21:56 -0800

    Ship small-screen named exports in a separate file.

---
+++ scripts/gen-exports.js
@@ -0,0 +1,14 @@
+/*
+ * Copyright (C) 2019 Daniel Anderson
+ *
+ * This source code is licensed under the MIT license found in the LICENSE file in the root directory of this source tree.
+ */
+
+console.log = () => {}
+
+const fs = require('fs')
+const path = require('path')
+const smallScreen = require(path.join(__dirname, '..'))
+const exportsPath = path.join(__dirname, '..', 'dist/exports.js')
+
+fs.writeFileSync(exportsPath, 'module.exports = ' + JSON.stringify(Object.keys(smallScreen)))
